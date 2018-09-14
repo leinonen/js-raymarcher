636ca9ef535e9acda8094eafcbfe0b7eb2795d09
Peter Leinonen <peter@leinonen.se>
2018-09-14 22:29:19 +0200

    cleanup

--- raymarcher.js
+++ raymarcher.js
@@ -1,7 +1,5 @@
-const iterations = 40;
-const E = 0.001;
-const clipFar = 10.0;
-const stepSize = 0.35;
+const iterations = 40, clipFar = 10.0, stepSize = 0.35, E = 0.001;
+const fov = .85, Ka = 0.1, Kd = 0.9, sp = 28.0;
 const { sin, cos, pow, abs, min, max, sqrt, PI } = Math
 const objectColor = [1, .8, 0];
 let time = 0;
@@ -14,12 +12,10 @@ function cross(a, b) { return [a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b
 function dot(a, b) { return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]) }
 function N(v) { return scale(v, 1.0 / length(v)) }
 function R(i, n) { return sub(i, scale(n, 2.0 * dot(n, i))) }
-function B(p, t) { return sin(p[0] * 16. + t * 0.57) * cos(p[1] * 16. + t * 2.17) * sin(p[2] * 16. - t * 1.31) + 0.5 * sin(p[0] * 32. + t * 0.07) * cos(p[1] * 32. + t * 2.11) * sin(p[2] * 32. - t * 1.23); }
 function rot(p, a) { let sa = sin(a); let ca = cos(a); return [p[0] * ca - p[1] * sa, p[0] * sa + p[1] * ca, p[2]]; }
-function sdSphere(p, radius) { return length(p) - radius; }
 function sdBox(p, b) { let d = sub([abs(p[0]), abs(p[1]), abs(p[2])], b); return min(max(d[0], max(d[1], d[2]))) + length([max(d[0], 0), max(d[1], 0), max(d[2], 0)]); }
 function sdCross(p, s) { return min(min(sdBox(p, [0.5, s, s]), sdBox(p, [s, 0.5, s])), sdBox(p, [s, s, 0.5])); }
-function fmod(num, m) { return ((num < 0) ? abs(m) : 0) + (num % m); }
+function fmod(n, m) { return ((n < 0) ? abs(m) : 0) + (n % m); }
 function mod(p, s) { return [fmod(p[0], s), fmod(p[1], s), fmod(p[2], s)] }
 function map(p) { return sdCross(sub(mod(p, 1.0), [.5, .5, .5]), 0.095); }
 
@@ -30,23 +26,18 @@ window.onload = function main() {
   let h = canvas.height = 80;
   document.body.appendChild(canvas)
   let ctx = canvas.getContext('2d')
-  let fov = .85;
   let lightPos = [0, 0, -3.5]
   let lookAt = [0, 0, 1.0]
   let camPos = [0, 0, -2.5]
   let forward = N(sub(lookAt, camPos));
   let right = N([forward[2], 0.0, -forward[0]]);
   let up = N(cross(forward, right));
-  let Ka = 0.1
-  let Kd = 0.9;
-  let sp = 128.0;
 
   function render() {
     time = Date.now() * .0006;
     lookAt[2] = time;
     camPos[2] = lookAt[2] - 2.5
     lightPos[2] = lookAt[2] - 3.5
-
     let imageData = ctx.createImageData(w, h);
     let data = imageData.data;
     let offs = 0;
@@ -73,13 +64,14 @@ window.onload = function main() {
         let final = [.2, .2, .2]
         if (t < clipFar) {
           let p = add(camPos, scale(rd, t));
-          let n = N([map([p[0] + E, p[1], p[2]]) - map([p[0] - E, p[1], p[2]]), map([p[0], p[1] + E, p[2]]) - map([p[0], p[1] - E, p[2]]), map([p[0], p[1], p[2] + E]) - map([p[0], p[1], p[2] - E])]);
+          let n = N([
+            map([p[0] + E, p[1], p[2]]) - map([p[0] - E, p[1], p[2]]), 
+            map([p[0], p[1] + E, p[2]]) - map([p[0], p[1] - E, p[2]]), 
+            map([p[0], p[1], p[2] + E]) - map([p[0], p[1], p[2] - E])
+          ]);
           let ld = N(sub(lightPos, p));
-          let len = length(ld);
-          ld = scale(ld, 1 / len);
-          let atten = min(1.0 / (0.225 * len * len), 1.0);
           let s = pow(max(0.0, dot(R(scale(ld, -1), n), N(sub(camPos, p)))), sp);
-          final = add(final, scale(objectColor, atten * (Ka + Kd * max(0.0, dot(n, ld)))));
+          final = add(final, scale(objectColor, Ka + Kd * max(0.0, dot(n, ld))));
           final = add(final, [s, s, s])
         }
         data[offs++] = final[0] * 255;
